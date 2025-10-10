949adb30e2edb5dbe717404cac974f141f8e0bea
Martin Winkler <martinwinkler1982@googlemail.com>
2025-10-10 22:05:37 +0200

    initial commit

---
+++ public/script/example.js
@@ -0,0 +1,36 @@
+import { go } from "second.js";
+
+const init = () => {
+  const scroller = document.getElementById("scroller");
+
+  const clearSnapped = () => {
+    [...scroller.children].forEach((element) => {
+      element.classList.remove("snapped");
+    });
+  };
+
+  const clearWillSnap = () => {
+    [...scroller.children].forEach((element) => {
+      element.classList.remove("will-snap");
+    });
+  };
+
+  scroller.addEventListener("scrollsnapchange", (e) => {
+    clearSnapped();
+    e.snapTargetInline?.classList.add("snapped");
+    window.setTimeout(() => {
+      clearWillSnap();
+    }, 1000);
+  });
+  scroller.addEventListener("scrollsnapchanging", (e) => {
+    clearWillSnap();
+    e.snapTargetInline?.classList.add("will-snap");
+  });
+
+  const select = document.getElementById("select");
+  select.addEventListener("change", (e) => {
+    document.body.style.setProperty("--scroll-snap-align", e.target.value);
+  });
+};
+
+if (go) init();
